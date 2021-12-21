bf732d4a1871049b91f3578e404063c14d6fafc2
saksham-tiwari <lucky121saty@gmail.com>
2021-12-22 01:04:30 +0530

    Started React Routes and login page.

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./components/Login";
+// import {Switch} from "react-router"
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+      <Route exact path="/login" element={<Login/>}/>
+      {/* <Route exact path="/home">
+        <p>homessss</p>
+        </Route> */}
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
